f26ec3d0b4adf81c1ad2d0ff130eb7893ef0d6eb
xxrunexx <rasyid.id3@gmail.com>
2021-12-27 17:08:25 +0700

    React Router and Creating new Login Views

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// Import dependencies
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Import views
+import login from './views/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          {/* <Route exact path='/' co2mponent={Home}/> */}
+          <Route exact path='/login' component={login}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 

ed9afd19e4939fd49a38d07e9f9a5fc991c2ad53
xxrunexx <rasyid.id3@gmail.com>
2021-12-29 18:08:09 +0700

    Fix Conflict

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Switch>
           {/* <Route exact path='/' component={Home}/> */}
           <Route exact path='/login' component={Login}/>
-          {/* <Route exact path='/home' component={Home}/> */}
         </Switch>
       </Router>
     </>

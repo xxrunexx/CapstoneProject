0097ec688580d60b01f9e4ee1566e0dd5ad44ada
crystalksy <dyahkinkinnn@gmail.com>
2022-01-05 12:01:48 +0700

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-// Import views
 import Login from './views/Login';
 import Home from './views/HomeDashboard/HomeDashboard';
 import Detailinvoice from './views/DetailInvoice/DetailInvoice';
 import Newinvoice from './views/NewInvoice/NewInvoice';
+import Register from './views/Register/Register';
+import NewPass from './views/NewPass/NewPass';
 
 function App() {
   return (
@@ -18,6 +18,8 @@ function App() {
           <Route exact path='/detail' component={Detailinvoice}/>
           <Route exact path='/addInvoice' component={Newinvoice}/>
           <Route exact path='/login' component={Login}/>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/newPass' component={NewPass}/>
         </Switch>
       </Router>
     </>
